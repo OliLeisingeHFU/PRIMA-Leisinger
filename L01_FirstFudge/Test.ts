d000e773c4ddab69b001bc4c3327f2936402b994
SG_Oliver_HFU <48796188+OliLeisingeHFU@users.noreply.github.com>
2021-03-26 14:33:09 +0100

    re

--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,16 +1,39 @@
+
 namespace L01_FirstFudge {
   import ƒ = FudgeCore;
   window.addEventListener("load", init);
-  
-  function init(_event: Event): void{
-	let node: ƒ.Node = new ƒ.Node("Test");
-	const canvas: HTMLCanvasElement = document.querySelector("canvas");
+  let node: ƒ.Node = new ƒ.Node("Test");
+  let viewport: ƒ.Viewport = new ƒ.Viewport();
 
-	let mesh: ƒ.MeshQuad = new ƒ.MeshQuad("Quad");
-  node.addComponent(new ƒ.ComponentMesh(mesh));
+  function init(_event: Event): void {
+    const canvas: HTMLCanvasElement = document.querySelector("canvas");
 
-  let material: ƒ.Material = new ƒ.Material("Bacon", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(0.5, 0, 0.75, 1)));
-  let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(material);
-  node.addComponent(cmpMaterial);
+    node.addComponent(new ƒ.ComponentTransform());
+
+    let mesh: ƒ.MeshQuad = new ƒ.MeshQuad("Quad");
+    node.addComponent(new ƒ.ComponentMesh(mesh));
+
+    let material: ƒ.Material = new ƒ.Material("Florian", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 1, 1, 1)));
+    let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(material);
+    node.addComponent(cmpMaterial);
+
+    let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    cmpCamera.mtxPivot.translateZ(3);
+    cmpCamera.mtxPivot.rotateY(180);
+    console.log(cmpCamera);
+
+    viewport.initialize("Viewport", node, cmpCamera, canvas);
+    viewport.draw();
+
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60);
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+  }
+
+  function update(_event: Event): void {
+    // console.log(_event);
+    let rotSpeed: number = 90;
+    let timeSinceLastFrameInSeconds: number = ƒ.Loop.timeFrameReal / 1000;
+    node.getComponent(ƒ.ComponentMesh).mtxPivot.rotateZ(rotSpeed * timeSinceLastFrameInSeconds);
+    viewport.draw();
   }
 }
